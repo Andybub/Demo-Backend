28cded951d469c5ec4ffdd8fba7bf62418de6758
Andy <andy@ya-ya.tw>
2024-04-14 17:38:44 +0800

    revert: "disable Redis"
    This reverts commit 210f675f318a6bc32848739d517758db4c70ebae.

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import Router from "koa-router";
-// import { Redis } from 'ioredis';
+import { Redis } from 'ioredis';
 
 const trackingStatus = ["Created", "Package Received", "In Transit", "Out for Delivery", "Delivery Attempted", "Delivered", "Returned to Sender", "Exception"];
 
@@ -8,10 +8,10 @@ export const router = new Router();
 
 const prisma = new PrismaClient();
 
-// const redis = new Redis({
-//     port: 6379, // redis服务器默认端口号
-//     host: '127.0.0.1' // redis服务器的IP地址
-// })
+const redis = new Redis({
+    port: 6379, // redis服务器默认端口号
+    host: '127.0.0.1' // redis服务器的IP地址
+})
 
 router.get('/fake', async (ctx) => {
     const { num } = ctx.query;
@@ -56,9 +56,8 @@ router.get('/fake', async (ctx) => {
 router.get("/query", async (ctx, next) => {
     const { sno } = ctx.query;
 
-    // console.log(await redis.ttl(String(sno)));
-    // let cacheResult = await redis.get(String(sno));
-    let cacheResult = null;
+    console.log(await redis.ttl(String(sno)));
+    let cacheResult = await redis.get(String(sno));
 
     if (cacheResult) {
         console.log("Cache hit");
@@ -87,7 +86,7 @@ router.get("/query", async (ctx, next) => {
           });
     
         if (result) {
-            // await redis.set(String(sno), JSON.stringify(result), 'EX', 3600);
+            await redis.set(String(sno), JSON.stringify(result), 'EX', 3600);
             ctx.body = result;
         } else {
             ctx.body = "No data found";
